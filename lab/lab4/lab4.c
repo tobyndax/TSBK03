01656d545ebdd9d85142478f1ea7942e2b32b855
Jens Edhammer <tobyndax@hotmail.com>
2015-09-30 14:06:21 +0200

    cohesion seems reasonable.

--- lab/lab4/lab4.c
+++ lab/lab4/lab4.c
@@ -4,12 +4,12 @@
 // OpenGL 3 conversion 2013.
 
 #ifdef __APPLE__
-	#include <OpenGL/gl3.h>
-	#include "MicroGlut.h"
-	// uses framework Cocoa
+#include <OpenGL/gl3.h>
+#include "MicroGlut.h"
+// uses framework Cocoa
 #else
-	#include <GL/gl.h>
-	#include "MicroGlut.h"
+#include <GL/gl.h>
+#include "MicroGlut.h"
 #endif
 
 #include <math.h>
@@ -20,72 +20,93 @@
 
 int numBoids = 0;
 float kAlignmentWeight = 1;
-float kCohesionWeight = 0.005f;
+float kCohesionWeight = 0.5f;
 float kAvoidanceWeight = 1;
-float kMaxDistance = 150;
+float kMaxDistance = 200;
 
 
-bool myDistance(SpritePtr sp1, SpritePtr sp2, float kMaxDistance){
+float myNorm(float a,float b ){
+	return sqrt(pow(a,2)+pow(b,2));
+}
+
+float myDistance(SpritePtr sp1, SpritePtr sp2){
 	float d1 = pow(sp2->position.h - sp1->position.h,2);
 	float d2 = pow(sp2->position.v - sp1->position.v,2);
 	//printf("%f\n",sqrt(d1+d2) );
-	return (sqrt(d1+d2)<kMaxDistance);
+	return sqrt(d1+d2);
 }
 
 
 void SpriteBehavior() // Din kod!
 {
-// L�gg till din labbkod h�r. Det g�r bra att �ndra var som helst i
-// koden i �vrigt, men mycket kan samlas h�r. Du kan utg� fr�n den
-// globala listroten, gSpriteRoot, f�r att kontrollera alla sprites
-// hastigheter och positioner, eller arbeta fr�n egna globaler.
+	// L�gg till din labbkod h�r. Det g�r bra att �ndra var som helst i
+	// koden i �vrigt, men mycket kan samlas h�r. Du kan utg� fr�n den
+	// globala listroten, gSpriteRoot, f�r att kontrollera alla sprites
+	// hastigheter och positioner, eller arbeta fr�n egna globaler.
 
-SpritePtr sp1 = gSpriteRoot;
-SpritePtr sp2;
+	SpritePtr sp1 = gSpriteRoot;
+	SpritePtr sp2;
 
-for (int i = 0; i < numBoids; ++i) {
-	int count = 0;
-	sp1->averagePosition.h = 0;
-	sp1->averagePosition.v = 0;
+	for (int i = 0; i < numBoids; ++i) {
+		int count = 0;
+		sp1->averagePosition.h = 0;
+		sp1->averagePosition.v = 0;
 
-	sp2 = gSpriteRoot;
-	for (int j = 0; j < numBoids; ++j) {
-		if (i == j){
+		sp1->avoidanceVector.h = 0;
+		sp1->avoidanceVector.v = 0;
 
-		}else if(myDistance(sp1,sp2,kMaxDistance)){
+		sp1->speedDiff.h = 0;
+		sp1->speedDiff.v = 0;
+
+		sp2 = gSpriteRoot;
+		for (int j = 0; j < numBoids; ++j) {
+			if (i == j){
+
+			}else if(myDistance(sp1,sp2) < kMaxDistance){// && myDistance(sp1,sp2) > 40  ){
 				sp1->averagePosition.h += sp2->position.h;
 				sp1->averagePosition.v += sp2->position.v;
 				count += 1;
+			}
+			sp2=sp2->next;
+		}
+
+		//update boid i's states
+		if (count > 0){
+			sp1->averagePosition.h /= count;
+			sp1->averagePosition.v /= count;
+			sp1->averagePosition.h -= sp1->position.h;
+			sp1->averagePosition.v -= sp1->position.v;
+			float coeff = sqrt(pow(sp1->averagePosition.h,2)+pow(sp1->averagePosition.v,2));
+			sp1->averagePosition.h /= coeff;
+			sp1->averagePosition.v /= coeff;
 
 		}
-		sp2=sp2->next;
+		//Go to next main boid
+		sp1 = sp1->next;
 	}
 
-	//update boid i's states
-	if (count > 0){
-		sp1->averagePosition.h /= count;
-		sp1->averagePosition.v /= count;
-	}else{
-		sp1->averagePosition.h = sp1->position.h;
-		sp1->averagePosition.v = sp1->position.v;
+
+	SpritePtr sp = gSpriteRoot;
+	for (int i = 0; i < numBoids; ++i) {
+		float speed = myNorm(sp->speed.h,sp->speed.v);
+		float coeff = myNorm(sp->speedDiff.h*kAlignmentWeight + (sp->averagePosition.h )*kCohesionWeight + sp->avoidanceVector.h *kAvoidanceWeight, sp->speedDiff.v*kAlignmentWeight + (sp->averagePosition.v) *kCohesionWeight + sp->avoidanceVector.v *kAvoidanceWeight);
+		if(coeff > 0){
+
+			sp->speed.h += (sp->speedDiff.h*kAlignmentWeight + (sp->averagePosition.h )*kCohesionWeight + sp->avoidanceVector.h *kAvoidanceWeight)/coeff;
+			sp->speed.v += (sp->speedDiff.v*kAlignmentWeight + (sp->averagePosition.v) *kCohesionWeight + sp->avoidanceVector.v *kAvoidanceWeight)/coeff;
+
+			float speed2 = myNorm(sp->speed.h,sp->speed.v);
+			sp->speed.h /= speed2;
+			sp->speed.h *= speed;
+			sp->speed.v /= speed2;
+			sp->speed.v *= speed;
+
+
+
+
+		}
+		sp = sp->next;
 	}
-
-	//Go to next main boid
-	sp1 = sp1->next;
-}
-SpritePtr sp = gSpriteRoot;
-for (int i = 0; i < numBoids; ++i) {
-	printf("%f \n", sp->averagePosition.h);
-	//sp->speed.h += sp->speedDiff.h*kAlignmentWeight + (sp->averagePosition.h )*kCohesionWeight + sp->avoidanceVector.h *kAvoidanceWeight;
-	//sp->speed.v += sp->speedDiff.v*kAlignmentWeight + (sp->averagePosition.v ) *kCohesionWeight + sp->avoidanceVector.v *kAvoidanceWeight;
-
-	sp->speed.h += sp->speedDiff.h*kAlignmentWeight + (sp->averagePosition.h -sp->position.h)*kCohesionWeight + sp->avoidanceVector.h *kAvoidanceWeight;
-	sp->speed.v += sp->speedDiff.v*kAlignmentWeight + (sp->averagePosition.v -sp->position.v) *kCohesionWeight + sp->avoidanceVector.v *kAvoidanceWeight;
-	sp = sp->next;
-}
-
-
-
 }
 
 // Drawing routine
@@ -103,7 +124,7 @@ void Display()
 
 	SpriteBehavior(); // Din kod!
 
-// Loop though all sprites. (Several loops in real engine.)
+	// Loop though all sprites. (Several loops in real engine.)
 	sp = gSpriteRoot;
 	do
 	{
@@ -132,56 +153,56 @@ void Timer(int value)
 float someValue = 0.0;
 
 void Key(unsigned char key,
-         __attribute__((unused)) int x,
-         __attribute__((unused)) int y)
-{
-  switch (key)
-  {
-    case '+':
-    	someValue += 0.1;
-    	printf("someValue = %f\n", someValue);
-    	break;
-    case '-':
-    	someValue -= 0.1;
-    	printf("someValue = %f\n", someValue);
-    	break;
-    case 0x1b:
-      exit(0);
-  }
-}
+	__attribute__((unused)) int x,
+	__attribute__((unused)) int y)
+	{
+		switch (key)
+		{
+			case '+':
+			someValue += 0.1;
+			printf("someValue = %f\n", someValue);
+			break;
+			case '-':
+			someValue -= 0.1;
+			printf("someValue = %f\n", someValue);
+			break;
+			case 0x1b:
+			exit(0);
+		}
+	}
 
-void Init()
-{
-	TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
+	void Init()
+	{
+		TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
 
-	LoadTGATextureSimple("bilder/leaves.tga", &backgroundTexID); // Bakgrund
+		LoadTGATextureSimple("bilder/leaves.tga", &backgroundTexID); // Bakgrund
 
-	sheepFace = GetFace("bilder/sheep.tga"); // Ett f�r
-	blackFace = GetFace("bilder/blackie.tga"); // Ett svart f�r
-	dogFace = GetFace("bilder/dog.tga"); // En hund
-	foodFace = GetFace("bilder/mat.tga"); // Mat
+		sheepFace = GetFace("bilder/sheep.tga"); // Ett f�r
+		blackFace = GetFace("bilder/blackie.tga"); // Ett svart f�r
+		dogFace = GetFace("bilder/dog.tga"); // En hund
+		foodFace = GetFace("bilder/mat.tga"); // Mat
 
-	NewSprite(sheepFace, 100, 200, 1, 1);
-	NewSprite(sheepFace, 200, 100, 1.5, -1);
-	NewSprite(sheepFace, 250, 200, -1, 1.5);
-}
+		NewSprite(sheepFace, 100, 200, 1, 1);
+		NewSprite(sheepFace, 200, 100, 1.5, -1);
+		//NewSprite(sheepFace, 250, 200, -1, 1.5);
+	}
 
-int main(int argc, char **argv)
-{
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(800, 600);
-	glutInitContextVersion(3, 2);
-	glutCreateWindow("SpriteLight demo / Flocking");
+	int main(int argc, char **argv)
+	{
+		glutInit(&argc, argv);
+		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+		glutInitWindowSize(800, 600);
+		glutInitContextVersion(3, 2);
+		glutCreateWindow("SpriteLight demo / Flocking");
 
-	glutDisplayFunc(Display);
-	glutTimerFunc(20, Timer, 0); // Should match the screen synch
-	glutReshapeFunc(Reshape);
-	glutKeyboardFunc(Key);
+		glutDisplayFunc(Display);
+		glutTimerFunc(20, Timer, 0); // Should match the screen synch
+		glutReshapeFunc(Reshape);
+		glutKeyboardFunc(Key);
 
-	InitSpriteLight();
-	Init();
+		InitSpriteLight();
+		Init();
 
-	glutMainLoop();
-	return 0;
-}
+		glutMainLoop();
+		return 0;
+	}
